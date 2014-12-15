04fef60aacf42b08a6a5fe67a6e657322f02e752
Aleksey <kvasilov48@users.noreply.github.com>
2014-12-15 14:46:05 +0200

    Update crontab.php

--- php/crontab.php
+++ php/crontab.php
@@ -12,14 +12,14 @@
 	 *	@package
 	 *
 	 * Example
-	  require_once('crontab.php');
-    $crontab=new crontab("/www/test/cron/", "filename");
-    $crontab->setDateParams(5, 10, 5, 5, "*");
-    $crontab->setCommand("curl http://www.mysite.com/?action=do_me");
-    $crontab->saveCronFile();
-    $crontab->addToCrontab();
-    $crontab->destroyFilePoint(); // OPTIONAL
-	  */
+	 * require_once('crontab.php');
+	 * $crontab=new crontab("/www/test/cron/", "filename");
+	 * $crontab->setDateParams(5, 10, 5, 5, "*");
+	 * $crontab->setCommand("curl http://www.mysite.com/?action=do_me");
+	 * $crontab->saveCronFile();
+	 * $crontab->addToCrontab();
+	 * $crontab->destroyFilePoint(); // OPTIONAL
+	 */
 	  class crontab{
 		
 		var $minute=NULL;
